f65b797b9f3fc51f066535293237848b0597a917
Roach1987 <31688489+Roach1987@users.noreply.github.com>
2020-01-20 22:12:13 +0100

    Declaración de atributos clase Cliente

--- src/ejercicio/clientes/Cliente.java
+++ src/ejercicio/clientes/Cliente.java
@@ -8,4 +8,11 @@ import java.io.Serializable;
  */
 public class Cliente implements Serializable{
     
+    // Declaramos el atributo para que la clase no pierda la serialización.
+    public static final long serialVersionUID = 1L;
+    private String nif;
+    private String nombre;
+    private int telefono;
+    private String direccion;
+    private double deuda;
 }
