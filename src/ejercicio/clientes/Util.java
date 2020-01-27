366935f35c13d5175bd946941b1954574cc68760
Roach1987 <31688489+Roach1987@users.noreply.github.com>
2020-01-27 22:09:59 +0100

    Cambios 27/01/2020

--- src/ejercicio/clientes/Util.java
+++ src/ejercicio/clientes/Util.java
@@ -1,6 +1,16 @@
 package ejercicio.clientes;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,11 +21,13 @@ import java.util.regex.Pattern;
  * @author Roach
  */
 public class Util {
+
     public static final String LETRAS_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+    public static final String MENSAJE_OK = "OK";
 // *************************************************************************************************************
 // *************************************** Métodos de validación ***********************************************
 // *************************************************************************************************************
-    
+
     /**
      * Método que comprueba que el teléfono es válido comienza por 6, 7 (Movil)
      * ó por 8, 9 (Fijo) y tiene 9 dígitos en total.
@@ -29,8 +41,9 @@ public class Util {
     }
 
     /**
-     * Metodo que valida un DNI, comprobando que su longitud sea correcta, asi como su Letra corresponde con
-     * el numero.
+     * Metodo que valida un DNI, comprobando que su longitud sea correcta, asi
+     * como su Letra corresponde con el numero.
+     *
      * @param nif
      * @return boolean
      */
@@ -51,11 +64,35 @@ public class Util {
         }
         return correcto;
     }
-    
+
+    /**
+     * Método que valida que la deuda no sea un numero negativo, ni una cadena
+     * de caracteres
+     *
+     * @param deuda
+     * @return
+     */
+    @SuppressWarnings("UnusedAssignment")
+    public static String compruebaDeuda(String deuda) {
+        String resultado;
+        int deudaNumerica = 0;
+        try {
+            if (null != deuda && !deuda.isEmpty()) {
+                deudaNumerica = Integer.parseInt(deuda);
+            } else {
+                resultado = "El campo deuda no esta informado.";
+            }
+            resultado = (deudaNumerica > 0) ? MENSAJE_OK : "La deuda no puede ser un numero negativo.";
+        } catch (NumberFormatException ex) {
+            resultado = "La deuda no es numerica.";
+        }
+        return resultado;
+    }
+
     /**
      * Metodo que pinta el menu de opciones que tendra disponible el usuario.
      */
-    public static void pintarMenu(){
+    public static void pintarMenu() {
         System.out.println("*** Menú principal ***");
         System.out.println("1 - Añadir cliente");
         System.out.println("2 - Listar clientes");
@@ -67,82 +104,210 @@ public class Util {
     }
 // *************************************************************************************************************
 // *************************************************************************************************************
-    
+
 // *************************************************************************************************************  
 // ************************************ Métodos del menu de usuario ********************************************
 // *************************************************************************************************************    
-   public static String introduceCliente(Scanner escanerEntrada){
+    public static String introduceCliente(Scanner escanerEntrada, File fichero) {
         boolean condicionDni = false;
         boolean condicionTelefono = false;
-        String nombreCliente, nif, direccion;
-        int telefono;
-        double deuda; 
-        
-       // Introducir nombre cliente.
-       System.out.println("*** Introduce el nombre del cliente ***");
-       nombreCliente = escanerEntrada.nextLine();
-       
-       // Introducir NIF
-       System.out.println("*** Introduce el DNI ***");
-       do{
-           String dniValidar = escanerEntrada.nextLine();
-           if(validarNIF(dniValidar)){
-               nif = dniValidar;
-               condicionDni = true;
-           }else{
-               System.out.println("*** El DNI introducido no es valido, vuelve a intentarlo. ***");
-           }
-       }while(!condicionDni);
-       
-       // Introducir Telefono
-       do{
-           String telefonoValidar = escanerEntrada.nextLine();
-           if(compruebaTelefono(telefonoValidar)){
-               telefono = Integer.parseInt(telefonoValidar);
-               condicionTelefono = true;
-           }else{
-               System.out.println("*** El telefono introducido no es valido, vuelve a intentarlo. ***");
-           }
-       }while(!condicionTelefono);
-       
-       // Introduce direccion
-       System.out.println("*** Introduce la dirección del cliente ***");
-       direccion = escanerEntrada.nextLine();
-       
-       // Introduce deuda.
-       
-       // continuar codigo *********************************************
-       
-       /// Crear metodo que valide la deuda.....
-       /// booleano que compruebe que la deuda no sea un String ni un numero negativo
-       //*****
-       //*****
-       
-       
-       
-       
-       /// Crear objeto tipo Cliente y guardarlo en el fichero **************************
-       //*******
-       //*******
-       //*******
-       //*******
-       
-       
-       
-       
-       return "Cliente creado correctamente";
-   }
-    
-    
+        boolean condicionDeuda = false;
+        String nombreCliente = "";
+        String nif = "";
+        String direccion = "";
+        int telefono = 0;
+        double deuda = 0.0;
+
+        // Introducir nombre cliente.
+        System.out.println("*** Introduce el nombre del cliente ***");
+        nombreCliente = escanerEntrada.nextLine();
+
+        // Introducir NIF
+        System.out.println("*** Introduce el DNI ***");
+        do {
+            String dniValidar = escanerEntrada.nextLine();
+            if (validarNIF(dniValidar)) {
+                nif = dniValidar;
+                condicionDni = true;
+            } else {
+                System.out.println("*** El DNI introducido no es valido, vuelve a intentarlo. ***");
+            }
+        } while (!condicionDni);
+
+        // Introducir Telefono
+        do {
+            String telefonoValidar = escanerEntrada.nextLine();
+            if (compruebaTelefono(telefonoValidar)) {
+                telefono = Integer.parseInt(telefonoValidar);
+                condicionTelefono = true;
+            } else {
+                System.out.println("*** El telefono introducido no es valido, vuelve a intentarlo. ***");
+            }
+        } while (!condicionTelefono);
+
+        // Introduce direccion
+        System.out.println("*** Introduce la dirección del cliente. ***");
+        direccion = escanerEntrada.nextLine();
+
+        // Introduce deuda.
+        System.out.println("*** Introduce la deuda del cliente. ***");
+        do {
+            String deudaEntrada = escanerEntrada.nextLine();
+            String deudaValidada = compruebaDeuda(deudaEntrada);
+            if (deudaValidada.equals(MENSAJE_OK)) {
+                deuda = Integer.parseInt(deudaEntrada);
+                condicionDeuda = true;
+            } else {
+                System.out.println("***  ***");
+            }
+        } while (!condicionDeuda);
+
+        // Instanciamos el cliente, con los datos recogidos.
+        Cliente clienteCreado = new Cliente(nif, nombreCliente, telefono, direccion, deuda);
+
+        // Comprobamos si el fichero esta abierto si no se abre, si no existe se crea.
+        // Añadimos la instancia del Cliente al fichero.
+        ficheroAnadirCliente(fichero, clienteCreado);
+
+        return "Cliente creado correctamente";
+    }
+
 // *************************************************************************************************************
 // *************************************************************************************************************
-    
-    
 // *************************************************************************************************************  
 // ****************************************** Métodos de Fichero ***********************************************
 // *************************************************************************************************************
-    
-    
+    /**
+     * Método que comprueba que el fichero existe si no lo crea, y a
+     * continuación añade el cliente al fichero
+     *
+     * @param fichero
+     * @param cliente
+     */
+    public static void ficheroAnadirCliente(File fichero, Cliente cliente) {
+        if (null != fichero && fichero.exists()) {
+            ficheroAnadir(fichero, cliente);
+        } else {
+            ficheroCrear();
+            ficheroAnadir(fichero, cliente);
+        }
+    }
+
+    /**
+     * Método que añade un nuevo cliente al fichero.
+     * @param fichero
+     * @param cliente 
+     */
+    public static void ficheroAnadir(File fichero, Cliente cliente) {
+        FileOutputStream ficheroSalida = null;
+        ObjectOutputStream objetoSalida = null;
+        try {          
+            ArrayList<Cliente> listaObjetosCliente = new ArrayList<Cliente>();
+                if (!fichero.exists()) {
+                    fichero = ficheroCrear();
+                }
+                FileInputStream ficheroFlujoEntrada = new FileInputStream(fichero); // lee la informacion del archivo
+                ObjectInputStream objetoFlujoEntrada = new ObjectInputStream(ficheroFlujoEntrada); // traduce la infromacion del archivo en datos
+                
+                listaObjetosCliente = (ArrayList<Cliente>) objetoFlujoEntrada.readObject(); // lee todos los objetos que esten en el array
+                ficheroFlujoEntrada.close();
+                objetoFlujoEntrada.close();
+                // traduce la infromacion del archivo en datos
+
+            listaObjetosCliente.add(cliente); // introduce en el array los datos del nuevo cliente contenidos en el objeto "datosCliente"
+            
+            ficheroSalida = new FileOutputStream(fichero); // Se crea el flujo para poder escribir en "clientes.dat"
+
+                objetoSalida = new ObjectOutputStream(ficheroSalida); // prepara la forma de escritura para "clientes.dat" que en este caso sera escribir un objeto
+                objetoSalida.writeObject(listaObjetosCliente);
+                
+            // Escribe en el archivo el Array de objetos "objetoArrayCliente"
+            // prepara la forma de escritura para "clientes.dat" que en este caso sera escribir un objeto
+                System.out.println("Cliente introducido con éxito");
+        } catch (FileNotFoundException | IOException | ClassNotFoundException ex) {
+            System.out.println("Error en el fichero: " + ex.getMessage());
+        } finally {
+            try {
+                if(null != ficheroSalida){
+                    ficheroSalida.close();
+                }
+                
+                if(null != objetoSalida){
+                    objetoSalida.close();
+                }
+            } catch (IOException ex) {
+                System.out.println("Error en el fichero: " + ex.getMessage());
+            }
+        }
+    }
+
+
+    /**
+     * Método que crea el fichero "clientes.dat"
+     *
+     * @return File
+     */
+    public static File ficheroCrear() {
+        return new File("clientes.dat");
+    }
+
+    /**
+     * Método que borra un fichero si este existe.
+     *
+     * @param fichero
+     */
+    public static void ficheroBorrar(File fichero) {
+        if (fichero.exists()) {
+            fichero.delete();
+        }
+    }
+
+    /**
+     * Método que imprime por pantalla el contenido del fichero, si este esta vacio imprime
+     * un mensaje indicativo.
+     * @param fichero 
+     */
+    public static void mostrarClientes(File fichero) {
+        try {
+            if (!fichero.exists()) {
+                fichero = ficheroCrear();
+            }
+            // con "isEmpty()"sabremos si el archivo tiene infromacion escrita o no
+            if (!cargarListaClientes(fichero).isEmpty()) {
+                ArrayList<Cliente> listaClientes = cargarListaClientes(fichero);
+                System.out.println("*** La lista de clientes es la siguiente: ***");
+                for (Cliente cliente : listaClientes) {
+                    System.out.println("***************************************************");
+                    cliente.toString();
+                    System.out.println("***************************************************");
+                }
+            } else {
+                System.out.println("El fichero no tiene ningun registro en este momento");
+            }
+        } catch (Exception e) {
+            System.out.println("Error al cargar el fichero: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Metodo que lee el fichero de flujo de entrada y devuelve una lista de
+     * cliente de este flujo.
+     *
+     * @param fichero
+     * @return
+     */
+    public static ArrayList<Cliente> cargarListaClientes(File fichero) {
+        ArrayList<Cliente> listaCliente = null;
+        try {
+            FileInputStream ficheroFlujoEntrada = new FileInputStream(fichero);// lee la informacion del archivo
+            ObjectInputStream objetoFlujoEntrada = new ObjectInputStream(ficheroFlujoEntrada); // traduce la informacion del archivo en datos       
+            listaCliente = (ArrayList<Cliente>) objetoFlujoEntrada.readObject(); // lee todos los objetos que esten en el array
+            // traduce la infromacion del archivo en datos
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("El fichero esta vacío");
+        }
+        return listaCliente;
+    }
 // *************************************************************************************************************
 // *************************************************************************************************************
 }
